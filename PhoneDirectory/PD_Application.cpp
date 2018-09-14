98e6cafafaaf1434c3fbae72b91093554850a0eb
Pragalva Dhungana <pdhungana21@my.whitworth.edu>
2018-09-14 16:39:04 -0700

    all done; confusion in ex 1.7

--- PhoneDirectory/PD_Application.cpp
+++ PhoneDirectory/PD_Application.cpp
@@ -87,9 +87,11 @@ void do_lookup_entry(Phone_Directory& the_directory)
 
 void do_remove_entry(Phone_Directory& the_directory) // Exercise 1.8: please complete the function do_remove_entry - Ed/Kent
 {
+	//Created by Pragalva Dhungana and Levi Russell
 	string name;
 	cout << "Enter name: ";
 	getline(cin, name);
+	the_directory.remove_entry(name);
 	// Complete the rest of this function 
 	
 }
